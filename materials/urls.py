6a0b61dc1ee7b8047d100fe16672403c284f70c0
Aliya Suleymanova <aliyasul2579@gmail.com>
2024-07-30 23:21:18 +0500

    Добавлены настройки переменного окружения

--- materials/urls.py
+++ materials/urls.py
@@ -12,9 +12,9 @@ router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='courses')
 
 urlpatterns = [
-    path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson-create'),
-    path('lesson/', LessonListAPIView.as_view(), name='lesson-list'),
-    path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson-get'),
-    path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson-update'),
-    path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson-delete'),
+    path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson_create'),
+    path('lesson/', LessonListAPIView.as_view(), name='lesson_list'),
+    path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson_get'),
+    path('lesson/<int:pk>/update/', LessonUpdateAPIView.as_view(), name='lesson_update'),
+    path('lesson/<int:pk>/delete/', LessonDestroyAPIView.as_view(), name='lesson_delete'),
 ] + router.urls
